38f4bb381ca5304856a97d1f5d6a31d0ed79c149
Jibrilh123 <jibrilh2009@gmail.com>
2025-01-04 20:23:24 +0300

    Add files via upload

---
+++ avg.py
@@ -0,0 +1,9 @@
+a = "Jibril"
+b = input("Enter your math marks")
+c = input("Enter your english marks")
+d = input("Enter your science marks")
+e = input("Enter your geo marks")
+f = input("Enter your history marks")
+sum = b+c+d+e+f
+avg= sum/5
+print(" The average of the 5 marks are", avg)
