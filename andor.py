b11c6be857088c94236530cbd91222980e68671e
Jibrilh123 <jibrilh2009@gmail.com>
2025-01-18 20:30:31 +0300

    Add files via upload

---
+++ andor.py
@@ -0,0 +1,19 @@
+a = 34
+b = 42
+c = 26
+if a and b and c:
+    print("all number have boolean value as true")
+else:
+    print("all number have boolean value as false")    
+
+a = 10
+b = -10
+c = 0
+if a>0 or b>0 : 
+    print("none of these number are greater than 0")
+else:
+    print("no number is greaater than 0")
+if b>0 or c>0 :
+    print("none of these number are greater than 0")    
+else:
+    print("no number is greater than 0")                
