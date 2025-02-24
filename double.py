dd5746a3e9911906a213f081126c67d6470e744a
Jibrilh123 <jibrilh2009@gmail.com>
2025-02-24 11:07:35 +0300

    Add files via upload

---
+++ double.py
@@ -0,0 +1,15 @@
+tuplex = ("tuple", False, 3.2, 1)
+print(tuplex)
+tuplex = (4,6,2,8,3,1)
+print(tuplex)
+tuplex = tuplex + (9,)
+print (tuplex)
+
+tuplel = (50,10,60,70,80,90)
+print(tuplel.count(50))
+
+tuplex = (2,1,5,4,8,6,9,2,0,3)
+slice1 = tuplex[3:5]
+print(slice1)
+slice1 = tuplex[:6]
+print(slice1)
---
+++ touple.py
@@ -0,0 +1,10 @@
+tup1 = (10, "Hello", True)
+print("Tuple 1:", tup1)
+
+tup2 = (1,2,3)
+tup3 = ("Python", "is", "fun")
+tup4 = tup2 + tup3
+print("Connected tuple:", tup4)
+
+tup5 = ("Repeat",)* 3
+print("Repeated tuple:", tup5)
