8a2d8dddbed3c657e0febc6ef6c23c443b1863a3
Jibrilh123 <jibrilh2009@gmail.com>
2025-02-28 11:24:32 +0300

    Add files via upload

---
+++ countrycode.py
@@ -0,0 +1,9 @@
+SPMcountry_code = {'India' : '0091',
+                   'Australia' : '0025',
+                   'Nepal' : '00977'}
+
+print("Country code for india - ")
+print(SPMcountry_code.get('India', 'not Found'))
+
+print("Country code for Japan -")
+print(SPMcountry_code.get('Japan', 'Not Found'))
---
+++ mirror.py
@@ -0,0 +1,18 @@
+def mirror(input,k):
+    original = 'abcdefghijklmnopqrstuvwxyz'
+    reverse = 'zyxwutsrqponmlkjihgfedcba'
+    dictChars = dict(zip(original, reverse))
+
+    prefix = input[0:k-1]
+    suffix = input[k-1:]
+    mirror = ''
+
+    for i in range(0, len(suffix)):
+        mirror = mirror + dictChars[suffix[i]]
+
+    print(prefix+mirror)
+
+if __name__=="__main__":
+    input = 'letter'
+    k = 3
+    mirror(input,k)
